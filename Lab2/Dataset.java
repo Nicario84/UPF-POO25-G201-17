171f865f8c3bd936ac3b205461ab4b40671d35fa
ADRIAN GARCIA U232258 <166702866+ADRIANGARCIAU232258@users.noreply.github.com>
2025-10-23 16:53:35 +0200

    Dataset.java

---
+++ Lab2/Dataset.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class Dataset {
+    private int dim;
+    private ArrayList<Record> data;
+
+    public Dataset(int d) {
+        this.dim = d;
+        this.data = new ArrayList<>();
+    }
+
+    public int getDim() {
+        return dim;
+    }
+
+    public ArrayList<Record> getData() {
+        return data;
+    }
+
+    public void addRecord(Record r) {
+        if (r.getInput().getElems().length != dim) {
+            System.out.println("La dimensiom del vector de entrada no coicide con la esperada");
+        }
+        data.add(r);
+    }
+
+    public Record getRecord(int i) {
+        return data.get(i);
+    }
+
+    public Vector meanInput() {
+        Vector sum = new Vector(dim, 0.0);
+        for (int i = 0; i < data.size(); i++) {
+            sum = sum.add(data.get(i).getInput());
+        }
+        return sum.divide(data.size());
+
+    }
+
+    public double meanOutput() {
+        double sum = 0.0;
+        for (int i = 0; i < data.size(); i++) {
+            sum += data.get(i).getOutput();
+        }
+        return sum / data.size();
+  }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Dataset:\n");
+        for (int i = 0; i < data.size(); i++) {
+            sb.append(data.get(i).toString()).append("\n");
+        }
+        return sb.toString();
+}
+
+}
